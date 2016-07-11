dbc2e071ea0bda549fd13b608b820f752494155f
Adrznej <a.korycinski@samsung.com>
2016-07-11 17:41:23 +0200

    add boolean converter

---
+++ nutcracker-server/src/main/java/com/adrznej/nutcracker/utils/converters/BooleanConverter.java
@@ -0,0 +1,22 @@
+package com.adrznej.nutcracker.utils.converters;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply=true)
+public class BooleanConverter implements AttributeConverter<Boolean, String>{
+    
+	@Override
+    public String convertToDatabaseColumn(Boolean value) {
+        if (Boolean.TRUE.equals(value)) {
+            return "T";
+        } else {
+            return "F";
+        }
+    }
+    
+    @Override
+    public Boolean convertToEntityAttribute(String value) {
+        return "T".equals(value);
+    }
+}
