d69528944218aecb67c3b9fd366d8c24b89ea11a
Rorschach2109 <rorschach2109@gmail.com>
2016-07-12 21:36:21 +0200

    Add method to setter remote interface and correct parameters of other method

--- nutcracker-server/src/main/java/com/nutcracker/remote/NutcrackerSetterRemote.java
+++ nutcracker-server/src/main/java/com/nutcracker/remote/NutcrackerSetterRemote.java
@@ -5,11 +5,14 @@ import javax.ejb.Remote;
 import com.nutcracker.model.NutCategory;
 import com.nutcracker.model.NutNote;
 import com.nutcracker.model.NutPlace;
+import com.nutcracker.model.NutUser;
 
 @Remote
 public interface NutcrackerSetterRemote {
 	
+	public int inserUser(NutUser user);
+	
 	public boolean insertCategory(int userId, NutCategory category);
-	public boolean insertNote(int userId, NutNote note);
+	public boolean insertNote(int userId, NutNote note, int categoryId, int placeId);
 	public boolean insertPlace(int userId, NutPlace place);
 }
