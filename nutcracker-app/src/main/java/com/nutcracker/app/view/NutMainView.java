3aeda601a6e44699a0dc6a0187ef03ccdf9d6daa
Adrznej <a.korycinski@samsung.com>
2016-07-20 14:51:22 +0200

    rename main view file

--- nutcracker-app/src/main/java/com/nutcracker/app/view/NutMainView.java
+++ nutcracker-app/src/main/java/com/nutcracker/app/view/NutMainView.java
@@ -1,14 +1,18 @@
 package com.nutcracker.app.view;
 
 import com.nutcracker.app.controller.INutController;
+import com.nutcracker.app.controller.NutMainController;
 
 import javafx.fxml.FXML;
 
-public class NutcrackerMainView implements INutView {
+public class NutMainView implements INutView {
+
+	private NutMainController mainController;
 	
 	@Override
 	public void setController(INutController controller) {
-		
+		this.mainController = (NutMainController) controller;
+		this.mainController.setView(this);
 	}
 	
 	@FXML
