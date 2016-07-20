b955c32d9b9413f817c061736c32c5bc8ec20d89
Adrznej <a.korycinski@samsung.com>
2016-07-20 09:09:23 +0200

    Add external class for text validation

---
+++ nutcracker-app/src/main/java/com/nutcracker/app/util/TextValidator.java
@@ -0,0 +1,24 @@
+package com.nutcracker.app.util;
+
+public final class TextValidator {
+
+	private TextValidator() {
+		
+	}
+	
+	public static boolean isEmpty(String text) {
+		return 0 == text.length();
+	}
+	
+	public static boolean isMultiCase(String text) {
+		return true;
+	}
+	
+	public static boolean hasNumber(String text) {
+		return true;
+	}
+	
+	public static boolean hasMinimumLength(String text, int minimumLength) {
+		return true;
+	}
+}
