f8c3dc1836a50eb6f9aa0e9ddb0d8ef3846ef3ac
Adrznej <a.korycinski@samsung.com>
2016-07-21 11:12:21 +0200

    Introduce more generic way for creating custom cells

--- nutcracker-app/src/main/java/com/nutcracker/app/controller/NutMainController.java
+++ nutcracker-app/src/main/java/com/nutcracker/app/controller/NutMainController.java
@@ -2,18 +2,29 @@ package com.nutcracker.app.controller;
 
 import java.util.List;
 
-import com.nutcracker.app.layout.NutNoteCell;
 import com.nutcracker.app.util.NutRemoteProxy;
 import com.nutcracker.app.view.INutView;
 import com.nutcracker.app.view.NutMainView;
 import com.nutcracker.model.NutNote;
 import com.nutcracker.remote.NutcrackerFinderRemote;
 
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
 public class NutMainController implements INutController {
 
 	private NutMainView mainView;
 	private final NutAppController nutAppController;
 	private final NutRemoteProxy remoteProxy;
+	
+
+	private static final String CELL_FACTORY_TEMPLATE;
+	
+	static {
+		CELL_FACTORY_TEMPLATE = "com.nutcracker.app.layout.%sCell";
+	}
 	
 	public NutMainController(NutAppController nutAppController, NutRemoteProxy remoteProxy) {
 		this.nutAppController = nutAppController;
@@ -24,18 +35,43 @@ public class NutMainController implements INutController {
 		NutcrackerFinderRemote nutFinder = remoteProxy.getNutFinder();
 		List<NutNote> notes = nutFinder.findNotesWithDeadline(nutAppController.getCurrentUserId());
 		
-		this.mainView.generateContent(notes, new NutNoteCell());
+		generateContent(notes, NutNote.class);
 	}
 	
 	public void generateNotesContent() {
 		NutcrackerFinderRemote nutFinder = remoteProxy.getNutFinder();
 		List<NutNote> notes = nutFinder.findNotesWithoutDeadline(nutAppController.getCurrentUserId());
 		
-		this.mainView.generateContent(notes, new NutNoteCell());
+		generateContent(notes, NutNote.class);
 	}
 	
 	@Override
 	public void setView(INutView view) {
 		this.mainView = (NutMainView) view;
+	}
+	
+	private <T> void generateContent(List<T> contentList, Class<T> classType) {
+		ListView<T> layoutList = createLayoutList(contentList, classType);
+		this.mainView.changeLayoutList(layoutList);
+	}
+
+	private <T> ListView<T> createLayoutList(List<T> contentList, Class<T> classType) {
+		ListView<T> layoutList = new ListView<>();
+		
+		layoutList.setCellFactory(new Callback<ListView<T>, ListCell<T>>() {
+            @SuppressWarnings("unchecked")
+			@Override public ListCell<T> call(ListView<T> list) {
+            	try {
+            		return (ListCell<T>) Class
+            				.forName(String.format(CELL_FACTORY_TEMPLATE, classType.getSimpleName()))
+            				.newInstance();
+            	} catch (Exception e) {
+            		return null;
+            	}
+            }
+        });
+		layoutList.setItems(FXCollections.observableArrayList(contentList));
+		
+		return layoutList;
 	}
 }
--- nutcracker-app/src/main/java/com/nutcracker/app/view/NutMainView.java
+++ nutcracker-app/src/main/java/com/nutcracker/app/view/NutMainView.java
@@ -1,29 +1,27 @@
 package com.nutcracker.app.view;
 
-import java.util.List;
-
 import com.nutcracker.app.controller.INutController;
 import com.nutcracker.app.controller.NutMainController;
-import com.nutcracker.app.layout.NutNoteCell;
-import com.nutcracker.model.NutNote;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
-import javafx.util.Callback;
 
 public class NutMainView implements INutView {
 
 	private NutMainController mainController;
-	
+		
 	@FXML
 	private AnchorPane contentPane;
 	
-	public void generateContent(List<NutNote> contentList, ListCell<NutNote> cellFactory) {
-		ListView<NutNote> layoutList = createLayoutList(contentList, cellFactory);
-		changeLayoutList(layoutList);
+	public <T> void changeLayoutList(ListView<T> layoutList) {
+		if (null == layoutList) {
+			return;
+		}
+		
+		this.contentPane.getChildren().clear();
+		layoutList.setMinSize(this.contentPane.getWidth(), this.contentPane.getHeight());
+		this.contentPane.getChildren().add(layoutList);
 	}
 	
 	@Override
@@ -50,24 +48,5 @@ public class NutMainView implements INutView {
 	@FXML
 	private void handleCategoriesButtonReleased() {
 		
-	}
-
-	private ListView<NutNote> createLayoutList(List<NutNote> contentList, ListCell<NutNote> cellFactory) {
-		ListView<NutNote> layoutList = new ListView<>();
-		
-		layoutList.setCellFactory(new Callback<ListView<NutNote>, ListCell<NutNote>>() {
-            @Override public ListCell<NutNote> call(ListView<NutNote> list) {
-                return new NutNoteCell();
-            }
-        });
-		layoutList.setItems(FXCollections.observableArrayList(contentList));
-		
-		return layoutList;
-	}
-	
-	private void changeLayoutList(ListView<NutNote> layoutList) {
-		this.contentPane.getChildren().clear();
-		layoutList.setMinSize(this.contentPane.getWidth(), this.contentPane.getHeight());
-		this.contentPane.getChildren().add(layoutList);
 	}
 }
